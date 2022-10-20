e1ec2c9e8678952f38081f4f4651cfd755c8daf7
Mario Ingelmo Diana <mid292@alumnos.unican.es>
2022-10-20 21:45:42 +0200

    Pequenhos cambios y rama finalizada

--- AndroidProject/app/src/androidTest/java/es/unican/is/appgasolineras/activities/filtroPermanente/FiltroPermanenteUITest.java
+++ AndroidProject/app/src/androidTest/java/es/unican/is/appgasolineras/activities/filtroPermanente/FiltroPermanenteUITest.java
@@ -39,16 +39,22 @@ public class FiltroPermanenteUITest {
     public void guardarYResetearFiltrosTest() {
         onView(withId(R.id.btnAccederFiltrosPermanentes)).perform(click());
         onView(withId(R.id.spinner_combustible)).perform(click());
+        // Selecciono en el spinner del combustible: Gasolina 95 E5
         onData(anything()).atPosition(1).perform(click());
         onView(withId(R.id.spinner_combustible)).check(matches(withSpinnerText("Gasolina 95 E5")));
         onView(withId(R.id.spinner_CCAA)).perform(click());
+        // Selecciono en el spinner de la comunidad: Cantabria
         onData(anything()).atPosition(1).perform(click());
         onView(withId(R.id.spinner_CCAA)).check(matches(withSpinnerText("Cantabria")));
+        // Guardo filtros
         onView(withId(R.id.btnGuardarPermanentes)).perform(click());
         onView(withId(R.id.btnAccederFiltrosPermanentes)).perform(click());
+        // Compruebo que los filtros han sido guardados
         onView(withId(R.id.spinner_combustible)).check(matches(withSpinnerText("Gasolina 95 E5")));
         onView(withId(R.id.spinner_CCAA)).check(matches(withSpinnerText("Cantabria")));
+        // Reseteo los filtros
         onView(withId(R.id.btnResetearPermanentes)).perform(click());
+        // Compruebo que los filtros se han reseteado
         onView(withId(R.id.spinner_combustible)).check(matches(withSpinnerText("Todos")));
         onView(withId(R.id.spinner_CCAA)).check(matches(withSpinnerText("Todas")));
     }
