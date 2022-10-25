88eb744a087fe78b6c1003d009721ed46d26b08f
Mario Ingelmo Diana <mid292@alumnos.unican.es>
2022-10-25 16:20:39 +0200

    Interfaz terminada

--- AndroidProject/app/src/main/java/es/unican/is/appgasolineras/activities/filtrar/FiltrarPorPrecioView.java
+++ AndroidProject/app/src/main/java/es/unican/is/appgasolineras/activities/filtrar/FiltrarPorPrecioView.java
@@ -9,9 +9,11 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 
 import es.unican.is.appgasolineras.R;
 import es.unican.is.appgasolineras.activities.main.MainView;
@@ -25,6 +27,9 @@ public class FiltrarPorPrecioView extends AppCompatActivity implements  IFiltrar
     Button btnResetear;
     Button btnMostrarResultados;
     EditText etPrecioLimite;
+    Spinner spnMarca;
+
+    IPrefs prefs;
 
     IFiltrarPorPrecioContract.Presenter presenter;
 
@@ -34,8 +39,8 @@ public class FiltrarPorPrecioView extends AppCompatActivity implements  IFiltrar
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filtrar_precio_view);
-        IPrefs prefs = Prefs.from(this);
-        getSupportActionBar().setTitle("Filtro Precio");
+        prefs = Prefs.from(this);
+        getSupportActionBar().setTitle(R.string.filtros);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         max = getIntent().getStringExtra("max");
@@ -50,6 +55,13 @@ public class FiltrarPorPrecioView extends AppCompatActivity implements  IFiltrar
     @SuppressLint("ClickableViewAccessibility")
     @Override
     public void init() {
+        //Configuracion del spinner de los combustibles
+        spnMarca = findViewById(R.id.spinner_marca);
+        ArrayAdapter<CharSequence> adapterMarcas = ArrayAdapter.createFromResource(this,
+                R.array.marcasArray, android.R.layout.simple_spinner_item);
+        adapterMarcas.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spnMarca.setAdapter(adapterMarcas);
+
         etPrecioLimite = findViewById(R.id.etPrecioLimite);
         etPrecioLimite.setText(max.substring(0,4));
 
@@ -75,11 +87,12 @@ public class FiltrarPorPrecioView extends AppCompatActivity implements  IFiltrar
         etPrecioLimite.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
+                // -
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // -
             }
 
             @Override
