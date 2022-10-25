d70e23201c9af93380fe817af259ad3f6b780817
Mario Ingelmo Diana <mid292@alumnos.unican.es>
2022-10-25 17:57:16 +0200

    Terminada rama feature/467590-BloquearVistaVerticalEnLaAplicacion

--- AndroidProject/app/src/main/java/es/unican/is/appgasolineras/activities/filtrar/FiltrarPorPrecioView.java
+++ AndroidProject/app/src/main/java/es/unican/is/appgasolineras/activities/filtrar/FiltrarPorPrecioView.java
@@ -75,13 +75,13 @@ public class FiltrarPorPrecioView extends AppCompatActivity implements  IFiltrar
         etPrecioLimite.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
+                // -
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
             }
-
+                // -
             @Override
             public void afterTextChanged(Editable s) {
                 if (s.toString().length() > 0) {
